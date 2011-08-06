d74a510112d7dd81161d972755f1d45b0de23106
Stefan Liebenberg <stefan@artspace44.com>
2011-08-06 12:36:51 +0200

    new files

--- examples/usage.js
+++
@@ -1,8 +0,0 @@
-Crontab.start();
-Crontab.add(new Cron( "@second" , function () { console.log( "Every Second" ) } ));
-Crontab.add(new Cron( "0 0 * * * *", function () { console.log( "Every Hour" )} ));
-Crontab.add(new Cron( "0 0 0 * * *", function () { console.log( "Every Day" ) } ));
- 
- 
-
-
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 files=lib/extentions.js lib/singletons/cronspec.js lib/classes/cron.js lib/singletons/crontab.js
 main: $(files)
 	mkdir build -p;
-	cat $(files) > build/jcron.js;
+	cat $(files) > build/cron.js;
