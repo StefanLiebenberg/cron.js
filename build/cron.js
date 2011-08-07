4dd5dd63c16ad576d4a22d524e5359380025f4c5
Stefan Liebenberg <stefan@artspace44.com>
2011-08-07 11:38:24 +0200

    update

--- build/cron.js
+++ build/cron.js
@@ -397,34 +397,48 @@ Cron.prototype = {
     this.getNextAt();
   }
 };
-var Crontab = new function () {
-  // ensure singleton;
-  if( this.constructor.instance ) return this.constructor.instance;
-  this.constructor.instance = this;
-  
-  var jobs = this.jobs = [];
-  
-  this.add = function( job ) {
-    jobs.push( job );
+var CronScheduler = function () {
+  this.jobs = [];
+  this.running = false;
+};
+
+
+CronScheduler.prototype = {
+  add: function ( job ) {
+    this.jobs.push( job );
     if( this.running ) {
       this.next();
+    };  
+  },
+  remove: function ( job ) {
+    var idx;    
+    while( ( idx = this.jobs.indexOf(obj) ) != -1 ) {
+      this.jobs.splice( idx );
     };
-  };
-  
-  this.remove = function ( job ) {
-    // todo, add remove code
-  };
-  
-  this.next = function () {
-    if( !this.running || this.storedTimeout || !jobs.length ) {
+  },
+  start: function () {
+    if(!this.running){
+      this.running = true;
+      this.next();
+    };
+  },
+  stop: function () {
+    if( this.running ) {
+      this.running = false;
+      clearTimeot( this.storedTimeout );
+      delete this.storedTimeout;
+    };
+  },
+  next: function () {
+    if( !this.running || this.storedTimeout || !this.jobs.length ) {
       return;
     };
     
-    var job = jobs[0];
-    for( var i = 1, l = jobs.length; i < l; i++ ) {
-      if( jobs[i].next_at < job.next_at ) {
-        job = jobs[i];
-      }
+    var job = this.jobs[0];
+    for( var i = 1, l = this.jobs.length; i < l; i++ ) {
+      if( this.jobs[i].next_at < job.next_at ) {
+        job = this.jobs[i];
+      };
     };
     
     var self = this;
@@ -433,19 +447,6 @@ var Crontab = new function () {
       delete self.storedTimeout;
       self.next();
     }, job.getTimeout() );
-  };
-  
-  this.start = function () {
-    if(! this.running ) {
-      this.running = true;
-      this.next();
-    }
-  };
-  this.stop = function () {
-    if(this.running ) {
-      this.running = false;
-      clearTimeout( this.storedTimeout );
-      delete this.storedTimeout;
-    };
-  };
+  }
 };
+var Crontab = new CronScheduler();;
