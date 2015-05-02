00eeaa5f151235d837c8ec6b375fd7a9effabcb1
Stefan Liebenberg <siga.fredo@gmail.com>
2015-05-02 21:22:58 +0200

    removing linter

--- library/src/test/javascript/slieb/util/range.test.js
+++ library/src/test/javascript/slieb/util/range.test.js
@@ -5,43 +5,45 @@ goog.require('slieb.util.Range');
 var range;
 
 function setUp() {
-  range = new slieb.util.Range(50, 99);
+    range = new slieb.util.Range(50, 99);
 }
 
 function tearDown() {
-  delete range;
+    delete range;
 }
 
 function testConstructor() {
-  assertTrue(range instanceof slieb.util.Range);
-  assertEquals(range.from, 50);
-  assertEquals(range.to, 99);
+    assertTrue(range instanceof slieb.util.Range);
+    assertEquals(range.from, 50);
+    assertEquals(range.to, 99);
 }
 
 function testLength() {
-  assertEquals(50, range.length);
+    assertEquals(50, range.length);
 }
 
 function testValueAt() {
-  var expected, actual;
-  for (var i = 0; i < 100; i++) {
-    expected = i < 50 ? i + 50 : undefined;
-    actual = range.valueAt(i);
-    assertEquals(expected, actual);
-  }
+    var expected, actual;
+    for (var i = 0; i < 100; i++) {
+        expected = i < 50 ? i + 50 : undefined;
+        actual = range.valueAt(i);
+        assertEquals(expected, actual);
+    }
 }
 
-function testIndexOf() {
+goog.exportSymbol("testIndexOf", function testIndexOf() {
+    var expected, actual;
 
-  var expected, actual;
+    for (var i = 0; i < 100; i++) {
+        expected = i < 50 ? -1 : i - 50;
+        actual = range.indexOf(i);
+        assertEquals(expected, actual);
+    }
+});
 
-  for (var i = 0; i < 100; i++) {
-    expected = i < 50 ? -1 : i - 50;
-    actual = range.indexOf(i);
-    assertEquals(expected, actual);
-  }
-}
-
-function testGetValues() {
-  assertArrayEquals([60, 61, 62, 63, 64], range.getValues(60, 64));
-}
+/**
+ * Check that range.getValues returns an array of values.
+ */
+goog.exportSymbol('testGetValues', function () {
+    assertArrayEquals([60, 61, 62, 63, 64], range.getValues(60, 64));
+});
